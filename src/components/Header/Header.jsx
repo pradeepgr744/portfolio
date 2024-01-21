dbd8ce7702f10928efc39d36e36cd52e29dd2e9c
pradeepgr744 <dseeeap2020@gmail.com>
2024-01-21 23:49:18 +0530

    Updated Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -86,7 +86,7 @@ const Header = () => {
                     `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-blue-500" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-blue-700 lg:p-0`
                   }
                   
-                  title='Projects'
+                  title='Blog'
                 >
                   Blog
                 </NavLink>
